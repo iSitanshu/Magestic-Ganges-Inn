88445bb83add5285330dc7256f672434979b0c2f
iSitanshu <Sitanshumishra18@gmail.com>
2025-04-07 19:37:07 +0530

    Frontend Setup finished + added React Router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
   )
 }
 
